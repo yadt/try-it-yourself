66a4dcaa204848bcdecd1aa24f1c780fafbf4198
mwolf <marcel.wolf@immobilienscout24.de>
2012-11-30 15:12:06 +0100

    ignoring generated content and worked around pidof problem

--- yadtshell/helloworld.sh
+++ yadtshell/helloworld.sh
@@ -4,6 +4,9 @@ set -e -E -u -C
 NAME=$(basename $0 .sh)
 TARGET=$NAME
 
+# TODO: some systems does not have /sbin in PATH, therefore we link it from /usr/bin
+[[ -x /usr/bin/pidof ]] || sudo ln -sf /sbin/pidof /usr/bin/pidof
+
 echo "$NAME - 1: generating target description file '$TARGET/target'"
 rm -rf $TARGET/*
 mkdir -p $TARGET
@@ -18,8 +21,10 @@ echo
 
 echo "$NAME - 2: querying status of newly generated target"
 (
+    echo "updating the local installation first, this may take a few minutes..."
 	set -x
 	cd $TARGET
+	yadtshell update
 	yadtshell status
 	set +x
 )
