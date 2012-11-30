9a8689d3dbefd700206e5a8efd63fc3388fdc0ee
mwolf <marcel.wolf@immobilienscout24.de>
2012-11-30 15:45:43 +0100

    setup prepares environment properly, pimped gitignore

--- yadtshell/setup.sh
+++ yadtshell/setup.sh
@@ -5,6 +5,9 @@ echo "installing yadtshell rpms"
 echo "(enter 'y' after reviewing yum actions)"
 sudo yum install ./*.rpm || sudo yum reinstall ./*.rpm || exit
 
+sudo mkdir /var/log/yadtshell
+sudo chmod 777 /var/log/yadtshell
+
 echo "done."
 
 
